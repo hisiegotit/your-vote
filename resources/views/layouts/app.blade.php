311ceb05c45f7dcad654a5a893bc3bbdf236d615
hisiegotit <hisie.work@gmail.com>
2023-12-04 23:14:02 +0700

    done idea form design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,9 +45,9 @@
             </a>
         </div>
     </header>
-    <main class="container mx-auto max-w-1000px flex">
+    <main class="container mx-auto max-w-custom flex">
         <div class="w-70 mr-5">
-            <div class="bg-white border-2 border-pink rounded-xl mt-16">
+            <div class="bg-white border-2 border-light-pink rounded-xl mt-16">
                 <div class="text-center px-6 py-2 pt-6">
                     <h3 class="font-semibold">Add an idea</h3>
                     <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!</p>
@@ -55,6 +55,32 @@
                 <form action="#" method="POST" class="space-y-4 px-4 py-6">
                     <div>
                         <input type="text" class="w-full bg-gray-100 border-none text-sm rounded-xl placeholder-gray-900 px-4 py-2" placeholder="Your idea">
+                    </div>
+                    <div>
+                        <select name="add_category" id="add_category" class="w-full bg-gray-100 border-none text-sm rounded-xl placeholder-gray-900 px-4 py-2">
+                            <option value="cate1">Category</option>
+                            <option value="cate2">Category 2</option>
+                            <option value="cate3">Category 3</option>
+                            <option value="cate4">Category 4</option>
+                         </select>
+                    </div>
+                    <div>
+                        <textarea name="idea" id="idea" cols="30" rows="10" class=" border-none w-full bg-gray-100 rounded-xl placeholder-gray-900 text-sm px-4 py-2" placeholder="Describe your idea"></textarea>
+                    </div>
+                    <div class="flex items-center justify-between space-x-3">
+                        <button type="button" class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                            <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-5 text-gray-600">
+                                <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
+                              </svg>
+                            <span class="ml-1"> Attach</span>
+                        </button>
+                        <button type="submit" class="flex items-center justify-center w-1/2 h-11 text-xs text-white bg-pink font-semibold rounded-xl border border-pink hover:border-strong-pink transition duration-150 ease-in px-6 py-3">
+                              <svg viewBox="0 0 24 24" fill="currentColor" class="w-4 h-5 transform -rotate-45">
+                                <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
+                              </svg>
+                                                           
+                            <span class="ml-1"> Submit</span>
+                        </button>
                     </div>
                 </form>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ export default {
                 xxs: ['0.625rem', { lineHeight: '1rem' }],
             },
             maxWidth: {
-                '1000px' : '62.5rem',
+                'custom' : '68.5rem',
             },
             boxShadow: {
                 card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
